122e80e61e5ad218f926d1a07d8a30f982482c1d
user.email <karlbecker@gmx.com>
2017-04-14 08:37:41 +0200

    change webpack dev file

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,36 +1,37 @@
 var webpack = require('webpack');
  
 module.exports = {
- 
-    /* webpack-dev-server를 콘솔이 아닌 자바스크립트로 실행 할 땐, 
-    HotReloadingModule 를 사용하기 위해선 dev-server 클라이언트와 
-    핫 모듈을 따로 entry 에 넣어주어야 합니다. */
-    
+	
+	/* when running webpack-dev-server with javascript not console,
+	for using HotReloadingModule, it is needed to insert dev-server client
+	and hot module to entry.
+	*/
+  
     entry: [
         './src/index.js',
-        'webpack-dev-server/client?http://0.0.0.0:4000', // 개발서버의 포트가 이 부분에 입력되어야 제대로 작동합니다
+        'webpack-dev-server/client?http://0.0.0.0:4000', // The port on the development server must be entered in this section to work properly
         'webpack/hot/only-dev-server'
     ],
  
     output: {
-        path: '/', // public 이 아니고 /, 이렇게 하면 파일을 메모리에 저장하고 사용합니다
+        path: '/', // "/" not "public", it stores files in memory to be used.
         filename: 'bundle.js'
     },
  
-    // 개발서버 설정입니다
+    // dev server settings
     devServer: {
         hot: true,
         filename: 'bundle.js',
         publicPath: '/',
         historyApiFallback: true,
         contentBase: './public',
-        /* 모든 요청을 프록시로 돌려서 express의 응답을 받아오며,
-        bundle 파일의 경우엔 우선권을 가져서 devserver 의 스크립트를 사용하게 됩니다 */
+		/* all requests are proxied to receive the response,
+		if it is bundle file, program use script of dev server. */
         proxy: {
-            "**": "http://localhost:3000" // express 서버주소
+            "**": "http://localhost:3000" // express server address
         },
         stats: {
-          // 콘솔 로그를 최소화 합니다
+          // minimize console log
           assets: false,
           colors: true,
           version: false,
